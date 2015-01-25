657cdf057dcdd851f3032786f4715e6e98d533b2
Luke Thomas <Lukert33@gmail.com>
2015-01-25 16:40:59 -0500

    ENV setup; add critical requirements.

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,12 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require 'dotenv' ; Dotenv.load ".env.local", ".env.#{Rails.env}"
+require 'dotenv'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
 
 module EchoThat
   class Application < Rails::Application
