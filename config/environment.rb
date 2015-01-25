ef350c020d39199f5f1c09a4bceb27e04cc9bd9e
Jorge Rubio C. & Mike Horn <pair+jorgerc85.mikeh0rn@devbootcamp.com>
2015-01-25 16:19:47 -0500

    Congif/Environment: Add config for Twitter client

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,11 @@ Rails.application.initialize!
 require 'dotenv'
 require 'EchoFactory'
 Dotenv.load
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER"]
+  config.consumer_secret     = ENV["TWITTER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
+
