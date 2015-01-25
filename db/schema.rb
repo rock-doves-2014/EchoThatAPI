ef692afbc18defdd6b2231799bf804c9335163c8
Jorge Rubio C. & Mike Horn <pair+jorgerc85.mikeh0rn@devbootcamp.com>
2015-01-25 16:22:03 -0500

    Users migration: add twitter oauth columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125065311) do
+ActiveRecord::Schema.define(version: 20150125203059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20150125065311) do
     t.boolean "facebook_on"
     t.integer "facebook_char_floor"
     t.boolean "always_link"
+    t.string  "twitter_token"
+    t.string  "twitter_token_secret"
   end
 
 end
