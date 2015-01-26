e2b5938cf22db41b3f4d61669a0503c781dd40d6
Luke Thomas <lukert33@gmail.com>
2015-01-26 16:43:30 -0500

    Specs; test datatype of params from front end.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  let(:params) do
+    {"{\"message\":\"ectations ships with a number of bui \",\"url\":\"http://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers\"}"=>nil, "controller"=>"echos", "action"=>"create"}
+  end
+
+  it "handles and reads params that convert into Json" do
+    hashtext = JSON.parse(params.first[0])
+    expect(hashtext).to be_a Hash
+    expect(hashtext["message"].length).to be > 5
+    expect(hashtext["url"].length).to be > 5
+    expect(hashtext["message"]).to be_a String
+  end
+
+  it "format the params into a hash that contains all required user attributes"
 
 end
