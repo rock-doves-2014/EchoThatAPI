52ca79e06bb661a6daa8e417dbeff51c7b3ebb9d
Jorge Rubio <jorgerc85@gmail.com>
2015-01-25 21:57:16 -0500

    Routes: Add auth/* routes for twitter Oauth handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  match 'auth/:provider/callback', to: 'users#create', via: 'get'
+  match 'auth/failure', to: redirect('/'), via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
