2d064b6baecc7b30fe7db3d864651717670f78cb
Luke Kedziora <lukekedziora@gmail.com>
2015-01-26 00:20:41 -0500

    getting the proper object in the create route

--- app/controllers/echos_controller.rb
+++ app/controllers/echos_controller.rb
@@ -1,6 +1,12 @@
 class EchosController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def create
+    p "*************************"
+    p params
+    p "*************************"
+
+
     echo = EchoFactory.construct_echo(params[:echo])
     echo.save
     render text: echo
