e112f14bf563b590282d78a1db725ad2fc951f8d
Luke Thomas <Lukert33@gmail.com>
2015-01-25 22:08:46 -0500

    Rails env; reconfigure a lot.

---
+++ app/models/echofactory.rb
@@ -0,0 +1,21 @@
+require 'bitly'
+module EchoFactory
+  def create_echo(json_obj)
+    json_obj.shortUrl = shorten_url(json_obj.url)
+    #more
+  end
+
+  def shorten_url(long_url)
+    clean_url = sanitize_url(long_url)
+    Bitly.use_api_version_3
+    #use bitly
+  end
+
+  def sanitize_url(url)
+    if /\?/.match(url) && !(/\/\?/.match(url))
+      return url.gsub("?","/?")
+    else
+      return url
+    end
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 require 'dotenv'
+require 'EchoFactory'
 Dotenv.load
