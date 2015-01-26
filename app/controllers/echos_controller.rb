954e27f09ff234158369ce9a4ca2d0dde05eb001
Luke Kedziora <lukekedziora@gmail.com>
2015-01-26 00:37:23 -0500

    Message passing; capture JSON from front end.

--- app/controllers/echos_controller.rb
+++ app/controllers/echos_controller.rb
@@ -2,10 +2,10 @@ class EchosController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    p "*************************"
-    p params
-    p "*************************"
+    hashtext = JSON.parse(params.first[0])
 
+    p hashtext["message"]
+    p hashtext["url"]
 
     echo = EchoFactory.construct_echo(params[:echo])
     echo.save
