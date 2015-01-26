84c3d8a715132e916a260a224144bb27ba9423c5
Luke Thomas <Lukert33@gmail.com>
2015-01-25 22:09:35 -0500

    Echo; sanitize jscn and create in db.

--- app/models/echofactory.rb
+++ app/models/echofactory.rb
@@ -1,21 +1,41 @@
-require 'bitly'
 module EchoFactory
-  def create_echo(json_obj)
-    json_obj.shortUrl = shorten_url(json_obj.url)
-    #more
+
+  def self.construct_echo(json_obj)
+    args = self.sanitize_json(json_obj)
+
+    echo = Echo.new(args[:echo])
+    return echo
   end
 
-  def shorten_url(long_url)
+  def self.sanitize_json(json_obj)
+    if json_obj.class == String
+      json_obj = JSON parse json_obj
+    end
+
+    json_obj[:long_url] = json_obj[:long_url]
+    json_obj[:short_url] = self.shorten_url(json_obj[:long_url])
+    #more once we figure out what is sent
+    json_obj = self.echo_params(json_obj)
+    safe_args = {echo: json_obj}
+    return safe_args
+  end
+
+  def self.shorten_url(long_url)
     clean_url = sanitize_url(long_url)
-    Bitly.use_api_version_3
-    #use bitly
+    return Bitly.client.shorten(long_url).short_url
   end
 
-  def sanitize_url(url)
+  def self.sanitize_url(url)
     if /\?/.match(url) && !(/\/\?/.match(url))
       return url.gsub("?","/?")
     else
       return url
     end
   end
+
+  def self.echo_params(json_hashified)
+    permitted = Echo.new.attributes.keys
+    return json_hashified.select{|k,v| permitted.include?(k.to_s)}
+  end
+
 end
