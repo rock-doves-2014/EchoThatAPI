110c8827b3dbd3abc6cdb4adf80d9325cf311aa2
Luke Thomas <lukert33@users.noreply.github.com>
2015-01-27 18:01:22 -0500

    Revert "Render text in chromepopup"

--- db/migrate/20150127181633_combine_echo_body_text.rb
+++
@@ -1,7 +0,0 @@
-class CombineEchoBodyText < ActiveRecord::Migration
-  def change
-    add_column :echos, :body, :text
-    remove_column :echos, :user_text, :text
-    remove_column :echos, :selected_string, :text
-  end
-end
