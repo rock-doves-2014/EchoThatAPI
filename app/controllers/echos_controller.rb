84ecf3ce46fc65d9dcad51363d383749a606d73d
Luke Thomas <lukert33@users.noreply.github.com>
2015-01-27 17:59:57 -0500

    Remove show for last echo for now.

--- app/controllers/echos_controller.rb
+++ app/controllers/echos_controller.rb
@@ -13,9 +13,4 @@ class EchosController < ApplicationController
     @echo = Echo.new
   end
 
-  def show
-    #will be User.get_by_credentials(params)
-    render text: "Placeholder for latest echo body"
-  end
-
 end
