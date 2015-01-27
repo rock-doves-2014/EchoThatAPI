7a81464bc28cfdf7b07ed6ddbfe14a2c6283e865
Luke Thomas <lukert33@gmail.com>
2015-01-26 22:54:11 -0500

    User; clean JSON with class .to_args method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,31 @@ class User < ActiveRecord::Base
   def twitter_history
     echos.select{|e| e.sent_to_venue.downcase == "twitter"}
   end
+
+  def self.to_args(json_params)
+    args = self.sanitize_json(json_params)
+    return args
+  end
+
+  private
+
+  def self.sanitize_json(json_params)
+    hash = JSON.parse(json_params.first[0])
+    safe_args = {}
+
+    safe_args[:google_credentials] = hash.fetch("google_credentials", nil)
+    safe_args[:twitter_on] = hash.fetch("twitter_on", true)
+    safe_args[:facebook_on] = hash.fetch("facebook_on", true)
+    safe_args[:facebook_char_floor] = hash.fetch("facebook_char_floor", true)
+    safe_args[:always_link] = hash.fetch("always_link", true)
+
+    user_args = self.user_params_only(safe_args)
+    {:user => user_args}
+  end
+
+  def self.user_params_only(json_hashified)
+    permitted = User.new.attributes.keys
+    return json_hashified.select{|k,v| permitted.include?(k.to_s)}
+  end
+
 end
