4c6fe8ecea6e63e87fffb380bdd4da1320a0ef98
Luke Thomas <lukert33@gmail.com>
2015-01-27 22:48:25 -0500

    Specs; working Echo#to_args test.

--- spec/models/echo_spec.rb
+++ spec/models/echo_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Echo, :type => :model do
 
-  pending "should produce a hash with default values to_args class method" do
+  let(:params){ {
+    "{\"message\":\"From Wikipedia, the free encyclopedia \",\"url\":\"https://en.wikipedia.org/wiki/Batman:_Arkham_City\",\"google_credentials\":\"104171572057165723377\",
+    \"chrome_token\":\"23imtculozn89v5fysbg470ep\"}"=>nil,
+     "controller"=>"echos",
+      "action"=>"create"}
+    }
+
+  it "should produce a hash with default values to_args class method" do
     args = Echo.to_args(params)
     defaults = [:is_draft, :body, :long_url]
-    expect(args.keys.include?(defaults)).to be true
+    defaults.each do |attrib|
+      expect(args.keys.include?(attrib)).to be true
+    end
   end
 end
