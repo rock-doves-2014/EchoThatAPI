807058f7d9a5c32bdfd550dd7ed53cb3ef31a203
Jorge Rubio <jorgerc85@gmail.com>
2015-01-27 23:44:24 -0500

    Schema: Include changes from last rebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127222743) do
+ActiveRecord::Schema.define(version: 20150127181633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20150127222743) do
     t.boolean "is_draft"
     t.text    "body"
   end
-
-  add_index "echos", ["short_url"], name: "index_echos_on_short_url", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string  "twitter_acct"
