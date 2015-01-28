0efa3e7c5a4afc1a3eb4fc37d824e57cbfb9a33a
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-28 11:52:54 -0500

    OmniAuth: Add Facebook configuration to initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 require 'omniauth-twitter'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end
