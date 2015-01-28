29d19a861e35846120ec441b53e458b061260ed9
Luke Thomas <lukert33@gmail.com>
2015-01-28 11:55:47 -0500

    Echo; string for venue to direct outlet assignment.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150128041557) do
+ActiveRecord::Schema.define(version: 20150128160203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +22,10 @@ ActiveRecord::Schema.define(version: 20150128041557) do
     t.string  "short_url"
     t.boolean "is_draft"
     t.text    "body"
-    t.string  "sent_to_account"
+    t.string  "send_to_venue"
   end
+
+  add_index "echos", ["short_url"], name: "index_echos_on_short_url", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string  "twitter_acct"
