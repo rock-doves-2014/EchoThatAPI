873438e8e93281e6e2b55f44c73ec4b53471cd94
Luke Thomas <lukert33@gmail.com>
2015-02-11 18:31:33 -0500

    Show new user page for only view.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'users#update', via: 'get'
   match 'auth/failure', to: redirect('/'), via: 'get'
 
-  root 'echos#new'
+  root 'users#new'
   post '/api/echos' => 'echos#create'
   post '/api/users' => 'users#create'
 
