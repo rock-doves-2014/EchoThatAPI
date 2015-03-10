8bd751f1489954273384c3c1ee0ea44887418138
Jorge Rubio <jorgerc85@gmail.com>
2015-03-10 17:32:32 -0400

    Controllers: Add custom token validation for before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+    def check_token
+      hashtext = JSON.parse(params.first[0])
+      user = User.find_by(google_credentials: hashtext['google_credentials'])
+      unless user.chrome_token == hashtext['chrome_token']
+        render status: 401
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_action :check_token, only: :update_sharing
 
   def new
 
