fa0a37664bcfa863ac5477d32b99679fae96eb37
Luke Thomas <lukert33@gmail.com>
2015-03-19 15:28:04 -0400

    Validate good email addresses.

--- app/models/support_email.rb
+++ app/models/support_email.rb
@@ -2,6 +2,7 @@ class SupportEmail < ActiveRecord::Base
   before_validation :add_recipient
   validates_presence_of :sender, :recipient, :body
   before_save :setup_mail
+  validate :email_format
 
   MAIL_OPTIONS = {
     :address       => "smtp.gmail.com",
@@ -32,6 +33,12 @@ class SupportEmail < ActiveRecord::Base
   def setup_mail
     Mail.defaults do
       delivery_method(:smtp, MAIL_OPTIONS)
+    end
+  end
+
+  def email_format
+    unless /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.match(sender)
+      errors.add(:sender, "must be a valid email address")
     end
   end
 
